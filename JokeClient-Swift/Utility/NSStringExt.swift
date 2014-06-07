a25df64e9639de2179e95a94c8d5bd0d6436eb3e
yangreal <yangreal@me.com>
2014-06-08 00:30:13 +0800

    bugfixed

---
+++ JokeClient-Swift/Utility/NSStringExt.swift
@@ -0,0 +1,44 @@
+//
+//  NSStringExtension.swift
+//  JokeClient-Swift
+//
+//  Created by YANGReal on 14-6-6.
+//  Copyright (c) 2014年 YANGReal. All rights reserved.
+//
+import UIKit
+import Foundation
+
+extension String {
+   
+
+    func stringHeightWith(fontSize:CGFloat,width:CGFloat)->CGFloat
+
+    {
+        var font = UIFont.systemFontOfSize(fontSize)
+        var size = CGSizeMake(width,CGFLOAT_MAX)
+       // var attr = [font:NSFontAttributeName]
+      
+
+        var paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineBreakMode = .ByWordWrapping;
+        var  attributes = [NSFontAttributeName:font,
+            NSParagraphStyleAttributeName:paragraphStyle.copy()]
+        
+        var text = self as NSString
+        var rect = text.boundingRectWithSize(size, options:.UsesLineFragmentOrigin, attributes: attributes, context:nil)
+      
+        return rect.size.height
+        
+    }
+    
+    func dateStringFromTimestamp(timeStamp:NSString)->String
+    {
+        var ts = timeStamp.doubleValue
+        var  formatter = NSDateFormatter ()
+        formatter.dateFormat = "yyyy年MM月dd日 HH:MM:ss"
+        var date = NSDate(timeIntervalSince1970 : ts)
+         return  formatter.stringFromDate(date)
+        
+    }
+    
+}
